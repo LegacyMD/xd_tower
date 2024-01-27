959e03331a504979d25960cfc44632c0e5bd8529
Dziuban <mdziuban@amd.com>
2024-01-27 02:14:12 +0100

    Add effect scene

---
+++ scenes/effect.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+signal effect_gathered(effect_type)
+
+enum EffectType {
+	Bounce,
+	SmashWithBlock,
+}
+
+func _on_player_entered(body):
+	print("Player gathered effect")
+
+	var effect_type = EffectType.keys()[randi() % EffectType.size()]
+	effect_gathered.emit(effect_type)
