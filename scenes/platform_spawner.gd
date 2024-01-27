d51404b85dfe6fda42959087e38c08157d0c9915
milliewalky <sovia@disroot.org>
2024-01-27 01:53:07 +0100

    Properly handle input for two players

--- scenes/platform_spawner.gd
+++ scenes/platform_spawner.gd
@@ -5,40 +5,40 @@ var tile_dimensions
 
 # Computes size of a platofrm tile in pixels
 func _compute_tile_size(player_view_dims):
-    var desired_tile_dim = 64 # Same is used for height and width
-    return Vector2i(desired_tile_dim, desired_tile_dim)
+	var desired_tile_dim = 64 # Same is used for height and width
+	return Vector2i(desired_tile_dim, desired_tile_dim)
 
 # Computes how many tiles fit horizontally and vertically on a single player_view
 func _compute_tile_dimensions(player_view_dims):
-    var tile_size = _compute_tile_size(player_view_dims)
-    var horizontal_tiles_num = player_view_dims.size.x / tile_size.x
-    var vertical_tiles_num = player_view_dims.size.y / tile_size.x
-    return Vector2i(horizontal_tiles_num, vertical_tiles_num)
+	var tile_size = _compute_tile_size(player_view_dims)
+	var horizontal_tiles_num = player_view_dims.size.x / tile_size.x
+	var vertical_tiles_num = player_view_dims.size.y / tile_size.x
+	return Vector2i(horizontal_tiles_num, vertical_tiles_num)
 
 func _spawn_tiles(tiles_dimensions, tile_size):
-    var root = get_parent()
-    
-    var tile = preload("res://scenes/platform_tile.tscn")
-    
-    for r in tiles_dimensions.y:
-        for c in tiles_dimensions.x:
-            var instance = tile.instantiate()
-            instance.position.x = (r * tile_size.x)
-            instance.position.y = (c * tile_size.y)
-            root.add_child.call_deferred(instance)
+	var root = get_parent()
+	
+	var tile = preload("res://scenes/platform_tile.tscn")
+	
+	for r in tiles_dimensions.y:
+		for c in tiles_dimensions.x:
+			var instance = tile.instantiate()
+			instance.position.x = (r * tile_size.x)
+			instance.position.y = (c * tile_size.y)
+			root.add_child.call_deferred(instance)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var player_view_node = get_node("../PlayerView").find_child("Background")
-    var player_view_dims = player_view_node.get_rect()
-    var tile_size = _compute_tile_size(player_view_dims)
-    var tiles_dimensions = _compute_tile_dimensions(player_view_dims)
-    print(tile_dimensions)
-    
-    _spawn_tiles(tiles_dimensions, tile_size)
-    pass # Replace with function body.
+	var player_view_node = get_node("../PlayerView").find_child("Background")
+	var player_view_dims = player_view_node.get_rect()
+	var tile_size = _compute_tile_size(player_view_dims)
+	var tiles_dimensions = _compute_tile_dimensions(player_view_dims)
+	print(tile_dimensions)
+	
+	_spawn_tiles(tiles_dimensions, tile_size)
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,17 +1,28 @@
 extends RigidBody2D
 
+# NOTE(mwk): for each player view, the index is hardcoded
+@export var player_idx : int
 
-# Called when the node enters the scene tree for the first time.
+func add_keyboard_mapping(action_name, keycode):
+    var input_event_key = InputEventKey.new()
+    input_event_key.physical_keycode = keycode
+    InputMap.add_action(action_name)
+    InputMap.action_add_event(action_name, input_event_key)
+
 func _ready():
-    var screen_size = get_viewport_rect().size
-    print(screen_size)
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+    # mwk: player -> keyboard mappings
+    add_keyboard_mapping("player0_up", KEY_W)
+    add_keyboard_mapping("player0_down", KEY_S)
+    add_keyboard_mapping("player0_left", KEY_A)
+    add_keyboard_mapping("player0_right", KEY_D)
+ 
+    add_keyboard_mapping("player1_up", KEY_UP)
+    add_keyboard_mapping("player1_down", KEY_DOWN)
+    add_keyboard_mapping("player1_left", KEY_LEFT)
+    add_keyboard_mapping("player1_right", KEY_RIGHT)
+    
 func _process(delta):
     pass
-
 
 func _on_area_2d_body_entered(body):
     print("JA PIERDOLE")
@@ -25,12 +36,20 @@ func is_on_ground() -> bool:
     return on_ground
 
 func _physics_process(delta):
+    # mwk: player index -> player name -> player inputs
+    var player_str = "%s%d" % ["player", player_idx]
+    var player_up = "%s_%s" % [player_str, "up"]
+    var player_down = "%s_%s" % [player_str, "down"]
+    var player_left = "%s_%s" % [player_str, "left"]
+    var player_right = "%s_%s" % [player_str, "right"]
+    
+    # mwk: calculate incidental velocity for left and right movements
     var input_vector = Vector2.ZERO
-    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-
+    input_vector.x = Input.get_action_strength(player_right) - Input.get_action_strength(player_left)
     if input_vector.length() > 0:
         input_vector = input_vector.normalized() * move_force
         apply_central_impulse(input_vector)
     
-    if Input.is_action_just_pressed("ui_up") and is_on_ground():
+    # mwk: handle jumping
+    if Input.is_action_just_pressed(player_up) and is_on_ground():
         apply_central_impulse(Vector2(0, jump_impulse))
