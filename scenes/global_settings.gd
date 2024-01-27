f9ab01d39f25a14acd40ea26177f25dcaad68759
Dziuban <mdziuban@amd.com>
2024-01-27 12:51:39 +0100

    Use CharacterBody2D for player

--- scenes/global_settings.gd
+++ scenes/global_settings.gd
@@ -5,9 +5,9 @@ var MIN_ROW_LENGTH: int = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
