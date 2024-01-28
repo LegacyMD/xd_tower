149c41fc7cab20861f7183ce7aa1b97ef3424394
Dziuban <mdziuban@amd.com>
2024-01-28 04:10:08 +0100

    Resize camera

--- scenes/root_scene.gd
+++ scenes/root_scene.gd
@@ -4,5 +4,14 @@ func _ready():
     $PlayerView.the_other_view = $PlayerView2
     $PlayerView2.the_other_view = $PlayerView
 
-func _process(delta):
-    pass
+    get_tree().get_root().size_changed.connect(resize)
+    resize()
+
+func resize():
+    var screen_size = get_viewport().get_visible_rect().size
+    var zoom_x = screen_size.x / 1152 # lol
+    var zoom_y = screen_size.y / 648 # kek
+    var zoom = min(zoom_x, zoom_y)
+    $Camera2D.zoom.x = zoom
+    $Camera2D.zoom.y = zoom
+    print("RESIZE %s, %s" % [screen_size.x, screen_size.y])
